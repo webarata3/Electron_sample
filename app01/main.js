94a12e8495f0b0435e1a6a2cf362d5c77e9777cf
webarata3 <webarata3@gmail.com>
2018-10-04 04:16:38 +0900

    セミコロン

--- app01/main.js
+++ app01/main.js
@@ -5,10 +5,9 @@ let win;
 function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
   win.loadFile('index.html');
-  win.webContents.openDevTools();
   win.on('closed', () => {
     win = null;
-  })
+  });
 }
 
 app.on('ready', createWindow);
