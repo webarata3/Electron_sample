982aa5e22737aec111ba8a2998eb6cf255eaac61
webarata3 <webarata3@gmail.com>
2018-10-06 14:17:04 +0900

    app04

---
+++ app04/main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({width:  800, height: 600, resizable: false});
+  win.loadFile('index.html');
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
