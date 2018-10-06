8096bedb7beb977b61e7dace209f88b8bd7cb7e4
webarata3 <webarata3@gmail.com>
2018-10-06 16:04:04 +0900

    app04

--- app04/main.js
+++ app04/main.js
@@ -1,10 +1,11 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width:  800, height: 600, resizable: false});
+  win = new BrowserWindow({width: 800, height: 600});
   win.loadFile('index.html');
+  initMenu();
   win.on('closed', () => {
     win = null;
   });
@@ -23,3 +24,104 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+function initMenu() {
+  const template = [
+    {
+      label: 'メニュー1',
+      submenu: [
+        {
+          label: '閉じる',
+          role: 'close'
+        },
+        {
+          label: 'サンプル1',
+          click() {
+          }
+        },
+        {type: 'separator'},
+        {
+          role: 'services', submenu: [
+            {
+              label: 'サンプル2',
+              click() {
+              }
+            },
+            {
+              label: 'サンプル3',
+              click() {
+              }
+            }
+          ]
+        },
+      ]
+    },
+    {
+      label: 'チェックボックス',
+      submenu: [
+        {
+          label: 'ログ出力',
+          click() {
+            const menu = Menu.getApplicationMenu();
+            const check1Menu = menu.getMenuItemById('check1');
+            const check2Menu = menu.getMenuItemById('check2');
+            console.log(`チェック1=${check1Menu.checked}`);
+            console.log(`チェック2=${check2Menu.checked}`);
+          }
+        },
+        {
+          id: 'check1',
+          label: 'チェック1',
+          type: 'checkbox'
+        },
+        {
+          id: 'check2',
+          label: 'チェック2',
+          type: 'checkbox'
+        }
+      ]
+    },
+    {
+      label: 'ラジオボタン',
+      submenu: [
+        {
+          label: 'ラジオ1',
+          type: 'radio'
+        },
+        {
+          label: 'ラジオ2',
+          type: 'radio'
+        },
+        {
+          label: 'ラジオ3',
+          type: 'radio'
+        },
+        {type: 'separator'},
+        {
+          label: 'ラジオ1',
+          type: 'radio'
+        },
+        {
+          label: 'ラジオ2',
+          type: 'radio'
+        },
+        {
+          label: 'ラジオ3',
+          type: 'radio'
+        }
+      ]
+    }
+  ];
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {role: 'about'}
+      ]
+    });
+  }
+
+    const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
