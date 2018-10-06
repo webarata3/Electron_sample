813756221cfaee1ebfeb34705e3d551803e00099
webarata3 <webarata3@gmail.com>
2018-10-06 14:14:18 +0900

    app03

--- app03/main.js
+++ app03/main.js
@@ -3,9 +3,8 @@ const {app, BrowserWindow} = require('electron');
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width:  800, height: 600, resizable: false});
   win.loadFile('index.html');
-  win.webContents.openDevTools();
   win.on('closed', () => {
     win = null;
   });
